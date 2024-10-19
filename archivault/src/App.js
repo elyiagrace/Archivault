12a8706899198c344a46f8286138b7cc1b5140b0
elyiagrace <elyiagrace@gmail.com>
2024-10-19 18:35:31 -0500

    Switched from npm to yarn.

--- archivault/src/App.js
+++ archivault/src/App.js
@@ -2,8 +2,17 @@
 import React from 'react';
 import './App.css';
 import FrontPage from './pages/FrontPage';
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+
 
 function App() {
+  <DynamicContextProvider
+    settings={{
+      environmentId: process.env.REACT_APP_DYNAMIC_ENVIRONMENT_ID,
+      walletConnectors: [ EthereumWalletConnectors ],
+    }}></DynamicContextProvider>
   return (
     <div className="App">
       <FrontPage />
